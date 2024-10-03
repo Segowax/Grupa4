ba907ef7d54c13f8b09969685e4b1e95953f5ad8
Segowax <kosmicznybandyta@gmail.com>
2024-10-03 20:00:25 +0200

    przygotowanie zadan

---
+++ WebApp/WebApp.Database/User.cs
@@ -0,0 +1,10 @@
+namespace WebApp.Database;
+
+public class User
+{
+    public int Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
---
+++ WebApp/WebApp.Server/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Bogus;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Database;
+
+namespace WebApp.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly Faker<User> _userFaker = new Faker<User>()
+            .RuleFor(u => u.Id, f => f.IndexFaker + 1)
+            .RuleFor(u => u.FirstName, f => f.Name.FirstName())
+            .RuleFor(u => u.LastName, f => f.Name.LastName())
+            .RuleFor(u => u.Email, (f, u) => f.Internet.Email(u.FirstName, u.LastName))
+            .RuleFor(u => u.DateOfBirth, f => f.Date.Past(30, DateTime.Now.AddYears(-18)));
+
+        public UserController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetAllUsers")]
+        public IEnumerable<User> Get() =>
+            _userFaker.Generate(10);
+    }
+}
